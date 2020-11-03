906789eba6234781bdba66f8f69e6cc8c8273ddb
valenovi76 <valentina.novara@bt.com>
2020-11-03 21:18:56 +0000

    fix to fixtures products file

--- products/models.py
+++ products/models.py
@@ -15,12 +15,11 @@ class Category (models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name: models.CharField(max_length=254)
-    price: models.DecimalField(max_digits=6, decimal_places=2)
-    qty: models.CharField(max_length=254)
-    description: models.CharField(max_length=254)
-    image_url: models.URLField(max_length=1024, blank=True)
-    image: models.ImageField(blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
