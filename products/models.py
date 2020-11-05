a3027240506e78256ae6ef29092cf2750c68e2c2
valenovi76 <valentina.novara@bt.com>
2020-11-05 16:19:05 +0000

    admin set to user friendly version

--- products/models.py
+++ products/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-# Create your models here.
 
-class Category (models.Model):
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -16,10 +19,10 @@ class Category (models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
+    qty = models.CharField(max_length=254, null=True, blank=True)
     def __str__(self):
         return self.name
