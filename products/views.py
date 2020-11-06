a23fa77842aad1203b96b71be5162318abb530f3
valenovi76 <valentina.novara@bt.com>
2020-11-06 22:20:52 +0000

    giltering by categories

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
@@ -6,28 +6,33 @@ from .models import Product, Category
 # Create your views here.
 
 def all_products(request):
-
-
-    """view to return all_products including sorting and querying all products """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
-            if 'q' in request.GET:
-                query = request.GET['q']
-                if not query:
-                    messages.error(request, "You didn't enter any search criteria!")
-                    return redirect(reverse('products'))
-                
-                queries = Q(name__icontains=query) | Q(description__icontains=query)
-                products = products.filter(queries)
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
-
+        'current_categories': categories,
     }
+
     return render(request, 'products/products.html', context)
 
 
