c2d839a7d6834511b182fb74270fbb1c29575d27
valenovi76 <valentina.novara@bt.com>
2020-11-08 16:21:15 +0000

    toast deployed successfully

--- bag/views.py
+++ bag/views.py
@@ -13,7 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    product = Products.object.get (pk=item_id)
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
