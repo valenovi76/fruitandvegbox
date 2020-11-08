1f52ddfd44aa95761a836c204e4af1e089a764e5
valenovi76 <valentina.novara@bt.com>
2020-11-08 11:38:48 +0000

    changes to bag

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -20,9 +20,10 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    qty = models.CharField(max_length=254, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, blank=True)
     image = models.ImageField(null=True, blank=True)
-    qty = models.CharField(max_length=254, null=True, blank=True)
+
     def __str__(self):
         return self.name
