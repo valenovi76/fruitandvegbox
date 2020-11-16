80c6750db7560d4036c7138a4e644060ab021a81
valenovi76 <valentina.novara@bt.com>
2020-11-16 14:47:24 +0000

    allowed host modified

--- fruitandvegbox/settings.py
+++ fruitandvegbox/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['veggnfruit.com', 'localhost']
+ALLOWED_HOSTS = ['veggnfruit.herokuapp.com', 'localhost']
 
 
 # Application definition
