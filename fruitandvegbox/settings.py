c092a2ed3ea66b7bdb7ad5204cca77367aa33518
valenovi76 <valentina.novara@bt.com>
2020-11-20 12:11:05 +0000

    newlsetter app and model created

--- fruitandvegbox/settings.py
+++ fruitandvegbox/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'newsletters'
 
     # Other
     'crispy_forms',
---
+++ newsletters/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from .models import NewsletterUsers
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_added',)
+
+
+admin.site.register(NewsletterUsers, NewsletterAdmin)
