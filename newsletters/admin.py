baa4bef4b711ce4f8e405ee12364c14140f33779
valenovi76 <valentina.novara@bt.com>
2020-11-21 15:47:03 +0000

    newsletter admin model migrated

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import NewsletterUser
+from .models import NewsletterUser, Newsletter
 
 
 class NewsletterAdmin(admin.ModelAdmin):
@@ -11,3 +11,5 @@ class NewsletterAdmin(admin.ModelAdmin):
 
 
 admin.site.register(NewsletterUser, NewsletterAdmin)
+
+admin.site.register(Newsletter)
--- newsletters/models.py
+++ newsletters/models.py
@@ -4,8 +4,26 @@ from django.db import models
 
 
 class NewsletterUser(models.Model):
+
     email = models.EmailField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
+
+
+class Newsletter(models.Model):
+    EMAIL_STATUS_CHOICES = (
+        ('Draft', 'Draft'),
+        ('Published', 'Published')
+    )
+    subject = models.CharField(max_length=250)
+    body = models.TextField()
+    email = models.ManyToManyField(NewsletterUser)
+    status = models.CharField(max_length=10, choices=EMAIL_STATUS_CHOICES)
+    created = models.TimeField(auto_now_add=True)
+    updated = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
+
