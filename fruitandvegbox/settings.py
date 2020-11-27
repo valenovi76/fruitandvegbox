82b12dfa707969f13043e0f56cc72c33140d4fd9
valenovi76 <valentina.novara@bt.com>
2020-11-27 12:52:54 +0000

    resyncing

--- fruitandvegbox/settings.py
+++ fruitandvegbox/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-#import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,8 +131,8 @@ if 'DATABASE_URL' in os.environ:
         }
 else:
 
-DATABASES = {
-    'default': {
+    DATABASES = {
+        'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
