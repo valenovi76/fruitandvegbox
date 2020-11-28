f90d2518acade3357a02201515fd9f578a9de445
valenovi76 <valentina.novara@bt.com>
2020-11-28 12:53:15 +0000

    added default from email

--- fruitandvegbox/settings.py
+++ fruitandvegbox/settings.py
@@ -213,11 +213,11 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'vegnfruitinabox@example.com'
+DEFAULT_FROM_EMAIL = 'vegnfruitinabox@gmail.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'vegnfruitinabox@example.com'
+    DEFAULT_FROM_EMAIL = 'vegnfruitinabox@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
